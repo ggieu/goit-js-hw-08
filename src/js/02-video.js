cd74d37341e5d8af72fad040308336ba9bdd3de9
eugene <j.golyanov9992@gmail.com>
2023-11-05 06:07:59 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,4 @@
-import player from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframeEl = document.querySelector('#vimeo-player');
 const player = new Player(iframeEl);
