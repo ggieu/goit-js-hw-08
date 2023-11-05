c81b82be6b6952a7dc2547778ea84054e4b3309d
eugene <j.golyanov9992@gmail.com>
2023-11-05 06:13:47 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
