7f6be0278ac1edd731a5364b91dcfd4a7f610e57
eugene <j.golyanov9992@gmail.com>
2023-11-05 06:10:24 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
 
-let lightbox;
-const gallery = document.querySelector(".gallery")
-const item = galleryItems.map(({preview, original, description}) => {
-    return `<li class ="gallery_item">
-    <a class="gallery_link" href="${original}">
-    <img
-    loading = "lazy"
-    class ="gallery_image"
-    src="${preview}"
-    alt ="${description}"
-    />
-    </a>
-    </li>`;
-}).join("");
-gallery.insertAdjacentHTML("beforeend", item);
-gallery.addEventListener("click", (evt) => evt.preventDefault())
 
+const listEl = document.querySelector('.gallery')
 
-lightbox = new SimpleLightbox('.gallery a', {
+const typset = galleryItems.map(({ preview, original, description }) => {
+    return `
+<li style="margin: 20px;list-style: none;" class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img loading="lazy" class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`}).join('');
+
+listEl.insertAdjacentHTML("beforeend", typset)
+
+let gallery = new SimpleLightbox('.gallery a', {
     captions: true,
     captionDelay: 250,
     fadeSpeed: 250,
@@ -31,4 +25,3 @@ lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionPosition: 'bottom',
 });
-console.log(galleryItems);
